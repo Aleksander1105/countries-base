9ef8b912ed555ba0b1a42167941777265bcffd5b
Aleksander Matyjewicz <matyjewiczaleksander@gmail.com>
2018-05-18 16:04:40 +0200

    Zadanie_20_4-DONE!

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
 import { getCountries } from './actions/actions-countries.js';
 import DevTools from './devTools';
+import routes from './routes';
 
 render(
 	<Provider store={store}>
-		<Router history={hashHistory}>
-			<Route path='/' component={Navigation}>
-
-			</Route>
-		</Router>
+		<Router history={hashHistory} routes={routes} />
 	</Provider>,
 	document.getElementById('root')
 ); 
---
+++ src/presentational/home.component.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Home = () => (
+	<h1>witamy w aplikacji Baza Państw</h1>
+);
+
+export default Home;
+	
